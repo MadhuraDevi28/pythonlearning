4570200c35f00dc698d16e6648dca0ede76a9ae7
madhu2628 <maduradevi3110@gmail.com>
2026-03-13 12:47:57 +0530

    understanding while loop

--- loop.py
+++ loop.py
@@ -1,2 +1,12 @@
-a=2%10
-print(a)
+# write a program to get sum of the previous number for the given input.
+# input=7
+# expected output=28
+
+n=7 #input
+i=1 #assign 1  to a variable to compare
+sum=0#initializing a variable
+while i<=n:#we while  loop to loop until our condition satisfy & we can neither use for loop also
+    sum=sum+i#here it sum the each i value until it reach the i<=n
+    i+=1#it increment the value of i
+print(sum)
+
